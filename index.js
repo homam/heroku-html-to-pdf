9cee6dc60aa7994476396816c7be03b3de300881
Homam <bluesnowball@gmail.com>
2016-08-02 14:42:12 +0200

    inital work

---
+++ index.js
@@ -0,0 +1,50 @@
+const app = require('express')();
+const http = require('http')
+const {exec} = require('shelljs')
+const fs = require('fs')
+const bodyParser = require('body-parser')
+const R = require('ramda')
+
+const binary = 'wkhtmltopdf' // './bin/wkhtmltopdf-linux-amd64'
+
+const port = process.env.PORT || 3001
+app.set('port', port);
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//app.use(busboy())
+
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
+app.use(multipartMiddleware)
+
+
+
+app.get('/', (req, res) => {
+  res.end('curl -F "html=@hello.html" "url"  > hello.pdf')
+})
+
+app.post("/", (req, res) => {
+
+  const htmlPath = req.files.html.path
+  const {code, stdout, stderr} = exec(
+    `${binary} -s Letter ${htmlPath} ${htmlPath}.pdf`
+  )
+
+  setTimeout(_ => {
+    exec(`rm ${htmlPath}`)
+    exec(`rm ${htmlPath}.pdf`)
+  }, 30000)
+
+  fs.createReadStream(`${htmlPath}.pdf`).pipe(res)
+  
+
+})
+
+const server = http.createServer(app)
+server.listen(port)
+
+console.log('started on ', port)
